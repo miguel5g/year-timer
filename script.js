eddd682c0a9bd239f2c148f2612d6a9102ccc829
Miguel Ângelo <miguelcg958@gmail.com>
2021-07-07 22:46:47 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const ONE_DAY = 1000 * 60 * 60 * 24,
+  ONE_HOUR = 1000 * 60 * 60,
+  ONE_MINUTE = 1000 * 60,
+  ONE_SECOND = 1000;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const timerDays = document.querySelector("div.timer-days strong"),
+    timerHours = document.querySelector("div.timer-hours strong"),
+    timerMinutes = document.querySelector("div.timer-minutes strong"),
+    timerSeconds = document.querySelector("div.timer-seconds strong");
+
+  function format(time, len = 2) {
+    return Math.floor(time).toString().padStart(len, "0");
+  }
+
+  function updateTimer() {
+    const { days, hours, minutes, seconds } = getTimerData();
+
+    timerDays.innerText = format(days, 3);
+    timerHours.innerText = format(hours);
+    timerMinutes.innerText = format(minutes);
+    timerSeconds.innerText = format(seconds);
+  }
+
+  const { seconds } = getTimerData();
+  updateTimer();
+  setTimeout(() => {
+    updateTimer();
+    setInterval(updateTimer, ONE_SECOND);
+  }, Math.floor((seconds - Math.floor(seconds)) * 1000));
+});
+
+function getTimerData() {
+  const timeLeft = new Date("2021", "11", "32").getTime() - Date.now();
+
+  const days = timeLeft / ONE_DAY;
+  const hours = (timeLeft % ONE_DAY) / ONE_HOUR;
+  const minutes = (timeLeft % ONE_HOUR) / ONE_MINUTE;
+  const seconds = (timeLeft % ONE_MINUTE) / ONE_SECOND;
+
+  return { days, hours, minutes, seconds };
+}
